b76e175befe5e67c04028e38403873a4828bd168
Глигоров Глигорчо <gligorcho.gligorov@students.finki.ukim.mk>
2023-12-24 13:35:09 +0100

    add impl

---
+++ backend/src/main/java/com/itlabs/backend/Service/Impl/ReportServiceImp.java
@@ -0,0 +1,57 @@
+package com.itlabs.backend.Service.Impl;
+
+import com.itlabs.backend.Service.ReportService;
+import com.itlabs.backend.models.Report;
+import com.itlabs.backend.models.User;
+import com.itlabs.backend.models.enums.ReportStatus;
+import com.itlabs.backend.models.enums.TrashType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReportServiceImp implements ReportService{
+
+    private final ReportService reportService;
+
+
+    public ReportServiceImp(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @Override
+    public List<Report> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Report> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Report> save(TrashType trashType, ReportStatus reportStatus, String description, User user, Double longitude, Double latitude) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Report> save(Report report) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Report> edit(Long id, TrashType trashType, ReportStatus reportStatus, String description, User user, Double longitude, Double latitude) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Report> edit(Long id, TrashType trashType) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
---
+++ backend/src/main/java/com/itlabs/backend/Web/ReportController.java
@@ -0,0 +1,11 @@
+package com.itlabs.backend.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reports")
+public class ReportController {
+
+    private final
+}
